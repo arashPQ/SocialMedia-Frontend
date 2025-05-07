ae33ce07afacb2bc8bea3eb692417cf34d13e902
arashPQ <arashpaqe@gmail.com>
2025-05-07 22:16:26 +0330

    Setup Front-end of project

---
+++ src/router/index.js
@@ -0,0 +1,35 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
+import SignupView from '@/views/SignupView.vue'
+import SigninView from '@/views/SigninView.vue'
+
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView,
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: AboutView,
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: SignupView,
+    },
+    {
+      path: '/signin',
+      name: 'signin',
+      component: SigninView,
+    },
+    
+  ],
+})
+
+export default router
